e8561696329b4a1da42424ac06322a77be63ca88
JxxHxxx <leesin5498@naver.com>
2024-05-15 06:42:49 +0900

    FIX 깃 충돌로 덮어쓰기로 저장

--- confirm-fe/src/api/memberApi.js
+++ confirm-fe/src/api/memberApi.js
@@ -3,25 +3,25 @@ import createAxiosInstance from "./GlobalApiConfiguration";
 const instance = createAxiosInstance('http://localhost:8080', true)
 
 export const getDeparmentMembers = function () {
-    
+
     const departmentId = sessionStorage.getItem('departmentId');
 
     const params = {
         cid: sessionStorage.getItem('companyId')
     }
 
-    return instance.get(`/api/departments/${departmentId}/member-leaves`, {params})
+    return instance.get(`/api/departments/${departmentId}/member-leaves`, { params })
         .then((res) => res.data)
         .catch((err) => alert(err))
 }
 
 export const searchDeparmentMembers = function (departmentId) {
-    
+
     const params = {
         cid: sessionStorage.getItem('companyId')
     }
 
-    return instance.get(`/api/departments/${departmentId}/member-leaves`, {params})
+    return instance.get(`/api/departments/${departmentId}/member-leaves`, { params })
         .then((res) => res.data)
         .catch((err) => alert(err))
 }
