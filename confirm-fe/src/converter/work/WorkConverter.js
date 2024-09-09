1d28e50ed1d959235d918e31710f5070410ceee1
JxxHxxx <leesin5498@naver.com>
2024-09-09 16:56:09 +0900

    FEAT 작업 티켓 상태 관련 메서드 개발

--- confirm-fe/src/converter/work/WorkConverter.js
+++ confirm-fe/src/converter/work/WorkConverter.js
@@ -1,18 +1,41 @@
 
 const workStatusEnum = {
-    CREATE : '티켓 생성',
-    RECEIVE : '티켓 접수',
-    ANALYZE_BEGIN : '내용 분석 시작',
-    ANALYZE_COMPLETE : '내용 분석 완료',
-    MAKE_PLAN_BEGIN : '계획 수립 시작',
-    MAKE_PLAN_COMPLETE : '계획 수립 완료',
-    REQUEST_CONFIRM : '결재 요청',
-    ACCEPT : '요청 부서 승인',
-    REJECT_FROM_REQUESTER : '요청 부서 반려',
-    REJECT_FROM_CHARGE : '담당 부서 반려',
-    WORKING : '작업 진행중',
-    DONE : '작업 완료',
-    DELETE : '티켓 삭제'
+    CREATE: '티켓 생성',
+    RECEIVE: '티켓 접수',
+    ANALYZE_BEGIN: '내용 분석 시작',
+    ANALYZE_COMPLETE: '내용 분석 완료',
+    MAKE_PLAN_BEGIN: '계획 수립 시작',
+    MAKE_PLAN_COMPLETE: '계획 수립 완료',
+    REQUEST_CONFIRM: '결재 요청',
+    ACCEPT: '요청 부서 승인',
+    REJECT_FROM_REQUESTER: '요청 부서 반려',
+    REJECT_FROM_CHARGE: '담당 부서 반려',
+    WORKING: '작업 진행중',
+    DONE: '작업 완료',
+    DELETE: '티켓 삭제'
+}
+
+const convertReceiveButtonName = (workStatus) => {
+    switch (workStatus) {
+        case 'CREATE':
+            return '티켓 접수';
+        case 'RECEIVE':
+            return '작업 분석 시작';
+        case 'ANALYZE_BEGIN':
+            return '내용 분석 완료';
+        case 'ANALYZE_COMPLETE':
+            return '계획 수립 시작';
+        case 'MAKE_PLAN_BEGIN':
+            return '계획 수립 완료';
+        case 'MAKE_PLAN_COMPLETE':
+            return '결재 요청';
+        case 'REQUEST_CONFIRM':
+            return '요청 부서 승인';
+        case 'ACCEPT':
+            return '작업 시작';
+        case 'WORKING' :
+            return '작업 완료';
+    }
 }
 
 const convertWorkStatus = (workStatus) => {
@@ -20,7 +43,8 @@ const convertWorkStatus = (workStatus) => {
 }
 
 const WorkConverter = {
-    convertWorkStatus
+    convertWorkStatus,
+    convertReceiveButtonName
 }
 
 
--- confirm-fe/src/page/work/receive/OneReceiveWorkTicketContent.jsx
+++ confirm-fe/src/page/work/receive/OneReceiveWorkTicketContent.jsx
@@ -3,14 +3,24 @@ import WorkApi from "../../../api/workApi"
 import { useParams } from "react-router-dom";
 import MainContainer from "../../../components/layout/container/MainContainer";
 import Title from "../../document/Title";
+import Table from "../../../components/table/Table";
+import WorkConverter from "../../../converter/work/WorkConverter";
+import Button from "../../../components/button/Button";
 
 
 export default function OneReceiveWorkTicketContent() {
     const params = useParams();
     console.log('params', params);
     const [oneWork, setOneWork] = useState({
-        workTicket : {},
-        workDetail : {}
+        workTicket: {
+            workTicketId: '',
+            workRequester: {
+                name: ''
+            },
+        },
+        workDetail: {
+            receiverName: ''
+        }
     });
 
     const fetchOneWorkTicket = async () => {
@@ -25,8 +35,7 @@ export default function OneReceiveWorkTicketContent() {
         }
     }
 
-    const {workTicket, workDetail} = oneWork;
-
+    const { workTicket, workDetail } = oneWork;
 
     useEffect(() => {
         fetchOneWorkTicket();
@@ -36,6 +45,20 @@ export default function OneReceiveWorkTicketContent() {
     return <MainContainer>
         <div id="oneReceiveWorkTicketContainer" style={{ border: '1px dashed blue', width: '900px', margin: '0px 0px 50px 0px', padding: '20px' }}>
             <Title name={'티켓 :' + workTicket.workTicketId} />
+            <Table tableProperty={
+                {
+                    columns: ['티켓 식별자', '처리 단계', '요청자', '담당 부서', '접수자'],
+                    data: <tr>
+                        <td value={workTicket.workTicketId}>{workTicket.workTicketPk}</td>
+                        <td>{WorkConverter.convertWorkStatus(workTicket.workStatus)}</td>
+                        <td>{workTicket.workRequester.name}</td>
+                        <td>{workTicket.chargeDepartmentId}</td>
+                        <td>{workDetail && workDetail.receiverName}</td>
+                    </tr>
+                }
+            } />
+            <Button cn="btn_normal"
+                name={WorkConverter.convertReceiveButtonName(workTicket.workStatus)} />
         </div>
     </MainContainer>
 }
